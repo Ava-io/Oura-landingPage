3c22a19380684c2e87d8fbec8ff20d3077d1d531
Ava.io <modupeoluwadorcas142@gmail.com>
2026-07-28 14:17:45 +0100

    section4 complete

--- src/components/Section4/Section4Componnent.tsx
+++ src/components/Section4/Section4Componnent.tsx
@@ -36,9 +36,47 @@ const Section4Componnent = () => {
         </div>
       </div>
 
-
       {/* seconnd div in section4 */}
-      <div></div>
+      <div className="grid grid-cols-3 mt-[200px]">
+        <div className=" gap-3 border-r-2 border-r-[#706d68]">
+          <div className="flex flex-col text-center">
+            <span className="lg:text-5xl text-[#706d68] font-thin font-serif text-5xl  leading-none">
+              25+ PhDs
+            </span>
+            <span className="leading-normal whitespace-break-spaces text-[#706d68] font-normal font-sans text-2xs tracking-normal">
+              working across an in-house <br />
+              interdisciplinary science team.
+            </span>
+          </div>
+        </div>
+
+        {/* seccond div */}
+        <div className="border-r-2 border-r-[#706d68]">
+          <div className="flex flex-col gap-2 text-center">
+            <span className="lg:text-5xl text-[#706d68] font-extralight font-serif text-5xl mb-2 leading-none">
+              4 Generations
+            </span>
+            <span className="leading-normal whitespace-break-spaces text-[#706d68] font-normal font-sans text-2xs tracking-normal">
+              with each one smarter, lighter,
+              <br /> and more durable than the last.
+            </span>
+          </div>
+        </div>
+
+        {/* third div */}
+        <div className="flex flex-col gap-2 text-center">
+          <span className="lg:text-5xl text-[#706d68] font-thin font-serif text-5xl mb-2 leading-none">
+            Millions of Oura <br />
+            Members
+          </span>
+          <span className="leading-normal whitespace-break-spaces text-[#706d68] font-normal font-sans text-2xs tracking-normal">
+            trust Oura to translate their body's <br />
+            messages into valuable, actionable <br /> insights.
+          </span>
+        </div>
+      </div>
+
+      <hr className="text-black/5 font-bold mt-[50px]" />
     </div>
   );
 };
